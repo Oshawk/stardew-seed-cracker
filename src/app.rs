5ad56462a508eee2816a7f4213ad4e6a82560d49
Oshawk <oshawk@protonmail.com>
2024-05-16 20:28:38 +0100

    Update heading

--- src/app.rs
+++ src/app.rs
@@ -173,7 +173,7 @@ impl Component for App {
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <section class="section">
-                <h1 class="title has-text-centered">{ "Stardew Valley Switch Seed Finder" }</h1>
+                <h1 class="title has-text-centered">{ "Stardew Seed Cracker" }</h1>
                 <h2 class="subtitle has-text-centered">{ "Enter the platform, date and first 10 items from the travelling merchant (order matters)." }</h2>
                 <div class="container">
                     <div class="columns">
